cc867d9235eae5fc1b44a0d7dd86c9beb4ee5a28
heyseppy <sepkimiaei1@gmail.com>
2023-06-14 22:48:44 +0800

    added simulation handler

---
+++ FirstSDLWindow/Simulation.cpp
@@ -0,0 +1,84 @@
+#include "Simulation.h"
+#include <stdio.h>
+
+Simulation::Simulation()
+{}
+
+Simulation::~Simulation()
+{}
+
+void Simulation::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+{
+	int flags = 0;
+
+	// check if user requested fullscreen
+	if (fullscreen)
+	{
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+
+	// runtime check to see whether SDL initialises correctly
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		std::cout << "Everything initialised" << std::endl;
+
+		window = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
+
+		if (window)
+		{
+			std::cout << "Window created" << std::endl;
+		}
+
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer)
+		{
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			std::cout << "Renderer created" << std::endl;
+		}
+
+		isRunning = true;
+	}
+	else 
+	{
+		isRunning = false;
+	}
+}
+
+void Simulation::update()
+{
+	counter++;
+	std::cout << counter << std::endl;
+}
+
+void Simulation::render()
+{
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+
+}
+
+void Simulation::handleEvents()
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type){
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+
+		default:
+			break;
+
+	}
+}
+
+void Simulation::clean()
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	std::cout << "Game cleaned" << std::endl;
+}
+
