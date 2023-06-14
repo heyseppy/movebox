10e098303f58cdd36c1dcee92bed4c9172ab313e
heyseppy <sepkimiaei1@gmail.com>
2023-06-14 22:48:20 +0800

    linked main

--- FirstSDLWindow/Main.cpp
+++ FirstSDLWindow/Main.cpp
@@ -1,17 +1,22 @@
-#include <stdio.h>
-#include "SDL.h"
+#include "Simulation.h"
+
+Simulation* simulation = nullptr;
 
 int main(int argc, char* argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	simulation = new Simulation();
 
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	SDL_RenderClear(renderer);
+	simulation->init("Movement Gym", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
-	SDL_RenderPresent(renderer);
-	SDL_Delay(3000);
+	while (simulation->running()) {
+
+		simulation->handleEvents();
+		simulation->update();
+		simulation->render();
+
+	}
+
+	simulation->clean();
 
 	return 0;
 }
